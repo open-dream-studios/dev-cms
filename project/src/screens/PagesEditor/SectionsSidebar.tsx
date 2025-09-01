6c17493dbf231eef3263e95c52521cbb428bbddb
JosephGoff <joeygoff13@gmail.com>
2025-09-01 16:33:54 -0400

    30

--- project/src/screens/PagesEditor/SectionsSidebar.tsx
+++ project/src/screens/PagesEditor/SectionsSidebar.tsx
@@ -1,8 +1,8 @@
-import { AuthContext } from '@/contexts/authContext';
-import { ProjectPage, Section } from '@/types/pages';
-import { appTheme } from '@/util/appTheme';
-import React, { useContext, useEffect, useState } from 'react'
-import { FiEdit } from 'react-icons/fi';
+import { AuthContext } from "@/contexts/authContext";
+import { ProjectPage, Section } from "@/types/pages";
+import { appTheme } from "@/util/appTheme";
+import React, { useContext, useEffect, useState } from "react";
+import { FiEdit } from "react-icons/fi";
 import {
   DndContext,
   closestCenter,
@@ -18,27 +18,45 @@ import {
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { useContextQueries } from '@/contexts/queryContext/queryContext';
-import { useProjectContext } from '@/contexts/projectContext';
-import { ContextInput, ContextInputType } from './PagesEditor';
+import { useContextQueries } from "@/contexts/queryContext/queryContext";
+import { useProjectContext } from "@/contexts/projectContext";
+import { ContextInput, ContextInputType } from "./PagesEditor";
 
 interface SectionsSidebarProps {
   filteredActiveSections: Section[];
   setEditingSection: React.Dispatch<React.SetStateAction<Section | null>>;
-  handleContextMenu: (e: React.MouseEvent, input: ContextInput, type: ContextInputType) => void;
+  handleContextMenu: (
+    e: React.MouseEvent,
+    input: ContextInput,
+    type: ContextInputType
+  ) => void;
 }
 
 interface SortableSectionItemProps {
   section: Section;
   setEditingSection: React.Dispatch<React.SetStateAction<Section | null>>;
-  handleContextMenu: (e: React.MouseEvent, input: ContextInput, type: ContextInputType) => void;
+  handleContextMenu: (
+    e: React.MouseEvent,
+    input: ContextInput,
+    type: ContextInputType
+  ) => void;
 }
 
-const SortableSectionItem = ({ section, setEditingSection, handleContextMenu }: SortableSectionItemProps) => {
-  const { currentUser } = useContext(AuthContext)
-  const { setCurrentSectionData } = useProjectContext()
+const SortableSectionItem = ({
+  section,
+  setEditingSection,
+  handleContextMenu,
+}: SortableSectionItemProps) => {
+  const { currentUser } = useContext(AuthContext);
 
-  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
     id: section.id,
   });
   const style: React.CSSProperties = {
@@ -49,39 +67,55 @@ const SortableSectionItem = ({ section, setEditingSection, handleContextMenu }: 
     zIndex: isDragging ? 9999 : "auto",
   };
 
-  if (!currentUser) return null
+  if (!currentUser) return null;
 
   return (
-
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className="w-full relative">
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="w-full relative"
+    >
       <div
         // onClick={() => {}}
         onContextMenu={(e) => handleContextMenu(e, section, "section")}
         className="dim hover:brightness-[85%] dim group cursor-pointer w-full h-[50px] flex justify-between items-center pl-[18px] pr-[12px] rounded-[8px]"
-        style={{ color: appTheme[currentUser.theme].text_4, backgroundColor: appTheme[currentUser.theme].background_1_2, }}
+        style={{
+          color: appTheme[currentUser.theme].text_4,
+          backgroundColor: appTheme[currentUser.theme].background_1_2,
+        }}
       >
-        <p className="select-none truncate w-[calc(100%-40px)]">{section.name}</p>
-        {currentUser.admin && <div
-          onClick={(e) => {
-            e.stopPropagation();
-            setEditingSection(section);
-          }}
-          className="hover:brightness-90 dim flex items-center justify-center min-w-[30px] w-[33px] h-[33px] rounded-full dim cursor-pointer"
-          style={{
-            backgroundColor:
-              appTheme[currentUser.theme].background_2_selected,
-          }}
-        >
-          <FiEdit size={15} />
-        </div>}
+        <p className="select-none truncate w-[calc(100%-40px)]">
+          {section.name}
+        </p>
+        {currentUser.admin && (
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              setEditingSection(section);
+            }}
+            className="hover:brightness-90 dim flex items-center justify-center min-w-[30px] w-[33px] h-[33px] rounded-full dim cursor-pointer"
+            style={{
+              backgroundColor:
+                appTheme[currentUser.theme].background_2_selected,
+            }}
+          >
+            <FiEdit size={15} />
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-const SectionsSidebar = ({ filteredActiveSections, handleContextMenu, setEditingSection }: SectionsSidebarProps) => {
-  const { currentUser } = useContext(AuthContext)
-  const { currentProjectId } = useProjectContext()
+const SectionsSidebar = ({
+  filteredActiveSections,
+  handleContextMenu,
+  setEditingSection,
+}: SectionsSidebarProps) => {
+  const { currentUser } = useContext(AuthContext);
+  const { currentProjectId, currentPage } = useProjectContext();
   const sensors = useSensors(
     useSensor(PointerSensor, {
       activationConstraint: { distance: 5 },
@@ -94,28 +128,42 @@ const SectionsSidebar = ({ filteredActiveSections, handleContextMenu, setEditing
     setLocalSections(filteredActiveSections);
   }, [filteredActiveSections]);
 
-  const { reorderProjectPages } = useContextQueries();
+  const { currentSection } = useProjectContext();
+  const { reorderSections } = useContextQueries();
 
   const handleDragEnd = async (event: DragEndEvent) => {
     const { active, over } = event;
     if (currentProjectId && over && active.id !== over.id) {
-      const oldIndex = localSections.findIndex((p: Section) => p.id === active.id);
-      const newIndex = localSections.findIndex((p: Section) => p.id === over.id);
+      const oldIndex = localSections.findIndex(
+        (p: Section) => p.id === active.id
+      );
+      const newIndex = localSections.findIndex(
+        (p: Section) => p.id === over.id
+      );
       const newOrder = arrayMove(localSections, oldIndex, newIndex);
       setLocalSections(newOrder);
 
-      // await reorderProjectSections({
-      //   project_idx: currentProjectId,
-      //   parent_page_id: selectedSection?.id ?? null,
-      //   orderedIds: newOrder.map(p => p.id),
-      // });
+      if (!currentPage) return;
+      await reorderSections({
+        project_idx: currentProjectId,
+        project_page_id: currentPage.id,
+        parent_section_id: currentSection ? currentSection.id : null,
+        orderedIds: newOrder.map((p) => p.id),
+      });
     }
   };
 
-  if (!currentUser) return null
+  if (!currentUser) return null;
   return (
-    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-      <SortableContext items={localSections.map((p: Section) => p.id)} strategy={verticalListSortingStrategy}>
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext
+        items={localSections.map((p: Section) => p.id)}
+        strategy={verticalListSortingStrategy}
+      >
         <div className="h-[100%] overflow-y-scroll flex flex-col gap-[9px]">
           {localSections.map((section: Section) => (
             <SortableSectionItem
@@ -128,7 +176,7 @@ const SectionsSidebar = ({ filteredActiveSections, handleContextMenu, setEditing
         </div>
       </SortableContext>
     </DndContext>
-  )
-}
+  );
+};
 
-export default SectionsSidebar
+export default SectionsSidebar;
