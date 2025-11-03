bc0bad0cec1b831be61321cab539e6a2c78782d3
JosephGoff <joeygoff13@gmail.com>
2025-11-03 03:52:41 -0500

    4

--- server/env.ts
+++ server/env.ts
@@ -7,7 +7,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Go up two levels: dist/server → server
-const envPath = path.resolve(__dirname, "../../server/.env");
+// const envPath = path.resolve(__dirname, "../../server/.env");
+const envPath = path.resolve(__dirname, "../.env");
 
 const result = dotenv.config({ path: envPath });
 if (result.error) console.error("⚠️ dotenv error:", result.error);
