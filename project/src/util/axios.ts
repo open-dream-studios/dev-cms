07c51e0aacae98dec46d9aa0c3640c7caa517d2c
JosephGoff <joeygoff13@gmail.com>
2025-12-05 10:38:22 -0500

    %

--- project/src/util/axios.ts
+++ project/src/util/axios.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 console.log("FINAL BACKEND URL USED:", process.env.NEXT_PUBLIC_BACKEND_URL);
 
 export const makeRequest = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: "",
   withCredentials: true,
 });
