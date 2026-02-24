8ab44078230c5b0aed2a4a758b462657c5ee5a31
JosephGoff <joeygoff13@gmail.com>
2026-02-24 10:42:35 -0800

    5

--- shared/types/models/payments.ts
+++ shared/types/models/payments.ts
@@ -1,7 +1,7 @@
 // shared/definitions/payments.ts
 
 export type CreditType = "1X_L1" | "1X_L2";
-export type SubscriptionType = "1M_L1" | "1M_L2" | "1M_L3";
+export type SubscriptionType = "6M_L1" | "6M_L2" | "6M_L3";
 export type StripeProduct = {
   price_id: string;
   mode: "payment" | "subscription";
@@ -25,19 +25,19 @@ export const stripeProducts = {
     credits: 10,
     title: "10 Credits",
   },
-  "1M_L1": {
+  "6M_L1": {
     price_id: "price_1T30EnQEiJzWYmC2CzLnn3aD",
     mode: "subscription",
     amount: "1.00",
     title: "1 Month | Level 1"
   },
-  "1M_L2": {
+  "6M_L2": {
     price_id: "price_1T30EvQEiJzWYmC2sqe3NS5U",
     mode: "subscription",
     amount: "5.00",
     title: "1 Month | Level 2"
   },
-  "1M_L3": {
+  "6M_L3": {
     price_id: "price_1T30F5QEiJzWYmC2ZHa4bgPi",
     mode: "subscription",
     amount: "10.00",
